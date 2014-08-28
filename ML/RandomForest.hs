eff22adfe2121146c439e67a0e1b71c526156cbd
daiver <ra22341@ya.ru>
2014-08-28 07:26:49 +0400

    minor changes

--- ML/RandomForest.hs
+++ ML/RandomForest.hs
@@ -18,7 +18,8 @@ trainRandomForest gen samples = RandomForest trees
     where 
         (seeds, _) = randomSeq gen 1
         ans = map (flip randomSamplingWithRepeats samples . mkStdGen) seeds
-        trees = map (\x -> buildForestNode ( fst $ shuffle [0..length (snd $ samples !! 0) - 1] (snd x)) (fst x)) ans
+        numOfFeats = length . head $ samples
+        trees = map (\x -> buildForestNode (take (numOfFeats/2) . fst $ shuffle [0..length (snd $ samples !! 0) - 1] (snd x)) (fst x)) ans
 
 buildForestNode featIdxs samples
     | bestGain > 0 = let (r, l) = divide samples featIdx thr in Node featIdx thr (buildNode l) (buildNode r)
