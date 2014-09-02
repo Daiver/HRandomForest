f24b00afa048459a0a894c8077fa735a68a89877
daiver <ra22341@ya.ru>
2014-09-02 21:30:44 +0400

    minor changes

--- main4.hs
+++ main4.hs
@@ -9,14 +9,15 @@ import GHC.IO.Exception
 --page = "http://rusfolder.com/random/images/?session=91b288d3fc0e8893e72536344e423636&mem"
 page = "http://rusfolder.com/random/images/?session=91b288d3fc0e8893e72536344e423636&mem"
 
-loadCommand = "wget -O images/%d.gif --wait=9 --random-wait %s"
+loadCommand = "curl '%s' >> images/%d.gif ; sleep 1"
+--loadCommand = "wget -O images/%d.gif --wait=9 --random-wait %s && touch file.is.done"
 convertComand = "convert -verbose -coalesce images/%d.gif images/%d.png"
 
 loadAndConvert :: Int -> IO GHC.IO.Exception.ExitCode
-loadAndConvert i = (waitForProcess =<< (runCommand $ (printf loadCommand (i::Int) page))) >> (waitForProcess =<< (runCommand $ (printf convertComand i i)))
+loadAndConvert i = (system $ (printf loadCommand page (i::Int) )) >> (system $ (printf convertComand i i))
 --loadAndConvert i = runCommand (printf loadCommand (i::Int) page) >> runCommand (printf convertComand i i)
 
 main = do
-    --mapM_ (loadAndConvert) [1..200]
-    mapM_ (\i -> waitForProcess =<< (runCommand $ (printf loadCommand (i::Int) page))) [1..200]
+    mapM_ (loadAndConvert) [1..500]
+    --mapM_ (\i -> waitForProcess =<< (runCommand $ (printf loadCommand (i::Int) page))) [1..2]
     --print $ proc "ls" []
