fbf65ef7ec1d6fa348fab8b652d82cc89a8ff5b5
daiver <ra22341@ya.ru>
2014-09-18 23:16:26 +0400

    add images read

--- mnist.hs
+++ mnist.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Data.Word
 import Data.Binary.Get
 import qualified Data.ByteString.Lazy as BStringLazy
@@ -11,13 +12,31 @@ listOfWord8 = do
             rest <- listOfWord8 
             return (v : rest)
 
+readListOfWord8N n = do
+    return . replicate (fromIntegral n) $ getWord8 
+
 readLabels = do
     magic <- getWord32be
     count <- getWord32be
-    list  <- listOfWord8 
+    --list  <- listOfWord8 
+    list <- readListOfWord8N count
     return (count, list)
 
+readImages = do
+    magic <- getWord32be
+    count <- getWord32be
+    
+    rows <- getWord32be
+    cols <- getWord32be
+    list <- replicateM (fromIntegral count) (readListOfWord8N (rows * cols))
+    return (count, (rows, cols), list)
+
 main = do
-    let fileName = "/home/daiver/Downloads/t10k-labels-idx1-ubyte"
-    content <- BStringLazy.readFile fileName
-    print $ runGet (readLabels) content
+    let labelsFileName = "/home/daiver/Downloads/t10k-labels-idx1-ubyte"
+    content <- BStringLazy.readFile labelsFileName
+    let(countOfLabels, labels) = runGet (readLabels) content
+    print $ (countOfLabels, length labels)
+    let imagesFileName = "/home/daiver/Downloads/t10k-images-idx3-ubyte"
+    content <- BStringLazy.readFile imagesFileName
+    let (s, (r, c), images) = runGet (readImages) content
+    print $ (s, r, c, length images) --}
