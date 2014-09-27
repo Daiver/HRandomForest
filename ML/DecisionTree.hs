976826cdb6336dd53a9255fe22426a0e992e88f6
daiver <ra22341@ya.ru>
2014-09-27 23:37:24 +0400

    minor changes

--- ML/DecisionTree.hs
+++ ML/DecisionTree.hs
@@ -5,6 +5,7 @@ import qualified Data.List as List
 import qualified Data.Map as Map
 import Control.Monad
 import Control.Arrow
+import Debug.Trace
 
 import ML.Common
 
@@ -42,11 +43,57 @@ buildNode samples
     | bestGain > 0 = let (r, l) = divide samples featIdx thr in Node featIdx thr (buildNode l) (buildNode r)
     | otherwise = Leaf . counts . map fst $ samples
     where 
-        (bestGain, featIdx, thr) = findBestThrBruteForce [0..length (snd $ samples !! 0) - 1] samples
+        (bestGain, featIdx, thr) = findBestThrIterative [0..length (snd $ samples !! 0) - 1] samples
+        --(bestGain, featIdx, thr) = findBestThrBruteForce [0..length (snd $ samples !! 0) - 1] samples
 
 predictDecTree :: DecisionTree  -> [Float] -> Int
 predictDecTree (Leaf vals) _ = fst . List.maximumBy (\x y -> compare (snd x) (snd y)) $ vals
 predictDecTree (Node featIdx thr l r) feats
     | feats !! featIdx >= thr = predict r feats
     | otherwise               = predict l feats
+
+
+
+{-New-}
+updateElem :: (a -> a) -> [a] -> Int -> [a]
+updateElem f li i = 
+    let(bef, aft) = splitAt i li in concat $ [bef, [f $ head aft], tail aft]
+
+findBestThrIterative :: [Int] -> [(Int, [Float])] -> (Float, Int, Float)
+findBestThrIterative featIndexes samples 
+    | length initWr > 1 = (bestGain, bestFeat, bestThr)
+    | otherwise = (0,0,0)
+    where 
+        (_, _, bestThr, bestFeat, bestGain) = foldl (foldlCore labels dt initGain) 
+              (initWl, initWr, dt !! 0 !! 0, 0, initGain) $ indexesAndFeats
+        labels = map fst samples
+        dt     = map snd samples
+        initWr = map snd $ counts labels
+        initWl = replicate (length initWr) 0
+        initGain = giniFromCounts . map snd . counts $ labels
+        countOfFeats = length $ dt !! 0
+        indexesAndFeats = concat $ map 
+                            (\f ->map ((,) f) $ List.sortBy 
+                                (\a b -> compare (dt!!a!!f) (dt!!b!!f)) 
+                                [0.. length dt - 1]) 
+                            featIndexes
+
+
+foldlCore labels dt initGain state@(wl, wr, bestThr, bestFeat, bestGain) (idx, feat)
+    | gain > bestGain = (wl', wr', thr, feat, gain)
+    | otherwise = (wl', wr', bestThr, bestFeat, bestGain)
+    where
+        l   = fromIntegral $ length labels
+        l1  = fromIntegral $ sum wl
+        l2  = fromIntegral $ sum wr
+        label = trace (show $ labels !! idx) $ labels !! idx
+        thr = dt !! idx !! feat
+        wl' = updateElem (+1) wl label
+        wr' = updateElem (\x -> x - 1) wr label
+        gain = initGain - giniFromCounts wl' * l1/l + giniFromCounts wr' * (l2/l)
+
+giniFromCounts :: [Int] -> Float
+giniFromCounts counts = 
+    1 - (fromIntegral (sum $ map (^2) counts) / fromIntegral (length counts ^ 2))
+
 
